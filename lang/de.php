60616ccc8af0ab95ff2c67dace613629ef716177
gitinsodema <github@insodema.de>
2026-08-28 17:24:50 +0200

    feat: prepare DE EN NL i18n foundation

---
+++ lang/de.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'nav.home' => 'Home',
+    'nav.services' => 'Leistungen',
+    'nav.audits' => 'Aktuelle Audits',
+    'nav.verify' => 'Verify',
+    'nav.ops' => 'OPS',
+    'nav.elite' => 'Elite Shopper',
+    'nav.about' => 'About',
+    'nav.contact' => 'Kontakt',
+    'footer.contact' => 'Kontakt',
+    'footer.navigation' => 'Navigation',
+    'footer.legal' => 'Rechtliches',
+    'footer.privacy' => 'Datenschutz',
+    'footer.imprint' => 'Impressum',
+    'footer.cookies' => 'Cookie-Einstellungen',
+];
