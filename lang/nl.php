94be29451dce8cc4c56833da1dac85c71eaaca3d
gitinsodema <github@insodema.de>
2026-08-28 22:15:03 +0200

    i18n: localize accessibility skip link

--- lang/nl.php
+++ lang/nl.php
@@ -1,5 +1,6 @@
 <?php
 return [
+    'accessibility.skip' => 'Naar hoofdinhoud',
  'nav.home'=>'Home','nav.services'=>'Diensten','nav.audits'=>'Actuele Audits','nav.verify'=>'Verify','nav.ops'=>'OPS','nav.elite'=>'Elite Shoppers','nav.about'=>'About','nav.contact'=>'Contact',
  'footer.contact'=>'Contact','footer.navigation'=>'Navigatie','footer.legal'=>'Juridisch','footer.privacy'=>'Privacy','footer.imprint'=>'Colofon','footer.cookies'=>'Cookie-instellingen',
  'cookie.title'=>'Privacy-instellingen','cookie.text'=>'We gebruiken alleen technisch noodzakelijke opslag voor veilige formulieren en om uw instellingen te onthouden. Analyse- of marketingcookies zijn momenteel niet actief.','cookie.necessary'=>'Alleen noodzakelijk','cookie.save'=>'Keuze opslaan',
