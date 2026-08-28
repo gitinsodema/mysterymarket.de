bc73c1905b50e0721eeaf49d5b83869e3737620c
gitinsodema <github@insodema.de>
2026-08-28 16:19:04 +0200

    feat: add MysteryMarket website V1 foundation

---
+++ config/local.example.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'db' => [
+        'dsn' => 'mysql:host=localhost;dbname=mysterymarket;charset=utf8mb4',
+        'user' => 'mysterymarket',
+        'password' => '',
+    ],
+    'legal' => [
+        'brand' => 'MysteryMarket',
+        'legal_form' => 'Einzelunternehmen',
+        'owner_name' => 'REPLACE_WITH_VERIFIED_OWNER_NAME',
+        'street' => 'REPLACE_WITH_VERIFIED_STREET',
+        'postal_code' => 'REPLACE_WITH_VERIFIED_POSTAL_CODE',
+        'city' => 'Düsseldorf',
+        'country' => 'Deutschland',
+        'email' => 'robert.breuss@mysterymarket.de',
+        'phone' => '+49 (0)152 223554659',
+        'vat_id' => '',
+        'small_business_regulation' => false,
+        'hosting_provider_name' => '',
+        'hosting_provider_address' => '',
+        'server_location' => 'Deutschland',
+        'privacy_supervisory_authority' => '',
+    ],
+];
