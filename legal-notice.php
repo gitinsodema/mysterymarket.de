56589114683cf6292e9d821778b15fcd28490c60
gitinsodema <github@insodema.de>
2026-08-30 14:47:33 +0200

    privacy: remove static public email addresses from legal-notice.php

--- legal-notice.php
+++ legal-notice.php
@@ -9,7 +9,7 @@ mmHeader($c['title'], $c['lead']);
 <section class="section">
 <div class="definition"><strong><?= mmEscape($c['provider']) ?></strong><div><strong><?= mmEscape((string)($legal['brand'] ?? 'MysteryMarket')) ?></strong><br><?= mmEscape((string)($legal['legal_form'] ?? 'Einzelunternehmen')) ?><br><?= mmEscape($c['owner']) ?>: <?= mmEscape((string)($legal['owner_name'] ?? '')) ?></div></div>
 <div class="definition"><strong><?= mmEscape($c['address']) ?></strong><div><?= mmEscape((string)($legal['street'] ?? '')) ?><br><?= mmEscape(trim((string)($legal['postal_code'] ?? '') . ' ' . (string)($legal['city'] ?? ''))) ?><br><?= mmEscape((string)($legal['country'] ?? 'Deutschland')) ?></div></div>
-<div class="definition"><strong><?= mmEscape($c['contact']) ?></strong><div>E-Mail: <a href="mailto:<?= mmEscape((string)($legal['email'] ?? 'hello@mysterymarket.de')) ?>"><?= mmEscape((string)($legal['email'] ?? 'hello@mysterymarket.de')) ?></a></div></div>
+<div class="definition"><strong><?= mmEscape($c['contact']) ?></strong><div>E-Mail: <?= mmEmailLink('hello', 'E-Mail') ?></div></div>
 <?php if (!empty($legal['vat_id'])): ?><div class="definition"><strong><?= mmEscape($c['vat']) ?></strong><div><?= mmEscape((string)$legal['vat_id']) ?></div></div><?php endif; ?>
 <?php if (($legal['small_business_regulation'] ?? false) === true): ?><div class="definition"><strong><?= mmEscape($c['tax']) ?></strong><div><?= mmEscape($c['small']) ?></div></div><?php endif; ?>
 </section>
